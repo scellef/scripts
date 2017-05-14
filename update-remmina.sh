a9829f5533ac7aa789ffe16055952a6dbd1a3819
Sean Ellefson <scellef@gmail.com>
2017-05-14 14:16:50 -0700

    Script to generate .remmina files to ganeti instances
    This will query a ganeti master's REST api to find which node the
    instance is currently living on, and then proceed to generate a .remmina
    file for an SSH tunneled VNC connection to the given instance.

---
+++ update-remmina.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Dependencies: jq, curl, genders
+ganetiUrl="https://example.com:5080/2/instances"
+remminaDir="${HOME}/.local/share/remmina"
+
+function error { IFS='\n' printf >&2 "[1;31mERROR: %s[0m\n" "$*" ;}
+function success { IFS='\n' printf >&2 "[1;32mSUCCESS: %s[0m\n" "$*" ;}
+function warning { IFS='\n' printf >&2 "[1;33mWARNING: %s[0m\n" "$*" ;}
+function prompt { IFS='\n' printf >&2 "[1;36m%s[0m\n" "$*" ;}
+function quit { prompt "Exiting..." ; exit 0 ;}
+
+if [ $@ ] ; then
+  instances=("$@")
+else
+  instances=( $(nodeattr -s virt=ganeti) )
+fi
+
+for instance in ${instances[*]} ; do 
+  server=$(curl -sk ${ganetiUrl}/${instance} | jq '[.pnode,.network_port|tostring]|join(":")')
+  if [ $server == "null:null" ] ; then
+    warning "$instance not found.  Is this a ganeti instance?"
+  else
+    sed -e "s/INSTANCE/$instance/" -e "s/SERVER/$server/" -e 's/"//g' << EOF > ${remminaDir}/${instance}.remmina \
+      && success "Remmina configuration file for '$instance' written!" \
+      || error "Could not write configuration file for '$instance'!"
+[remmina]
+keymap=
+ssh_auth=2
+quality=9
+disableencryption=0
+postcommand=
+ssh_privatekey=${HOME}/.ssh/id_rsa
+viewmode=1
+ssh_charset=UTF-8
+window_maximize=0
+password=
+group=Ganeti
+name=INSTANCE
+precommand=
+proxy=
+ssh_username=$USER
+ssh_loopback=1
+viewonly=0
+disableclipboard=0
+protocol=VNC
+ssh_server=
+window_width=874
+window_height=875
+ssh_enabled=1
+username=$USER
+showcursor=0
+disablepasswordstoring=0
+colordepth=32
+server=SERVER
+disableserverinput=0
+EOF
+  fi
+done
+
+quit

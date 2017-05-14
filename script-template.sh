3db73160f33c63def0842536c1c271891d5bbedd
Sean Ellefson <scellef@gmail.com>
2017-05-14 14:32:10 -0700

    Added generic parse_arguments

--- script-template.sh
+++ script-template.sh
@@ -4,18 +4,20 @@
 
 ### VARIABLES ###
 IFS=','
-variableName=''
-
+array=()
+variable=''
+flag=''
 
 ### DOCUMENTATION ###
 function usage {
-# Print usage information
   cat << EOF
 Usage: script-template.sh -f mandatory-parameter [ -o optional-parameter ]
+       script-template.sh -n
 
 Options:
   -f    Parameter that must be supplied
   -o    Parameter that may be supplied
+  -n    Parameter without argument
 
 Notes:
   Information on edge-cases and limitations
@@ -38,6 +40,29 @@ function check_dependencies {
   command -v color-test.sh 2> /dev/null >&2 || error "'color-test' not in your PATH."
 }
 
+function parse_arguments {
+  if [ $# -eq 0 ] ; then
+    prompt_for_info
+  elif [ "$1" == '-' ] ; then
+    error "Unknown option: $1" ; usage ; exit 2
+  elif [[ ! ($1 =~ "-") ]] ; then
+    error "Unknown option: $1" ; usage ; exit 2
+  else
+    while getopts ":o:f:nh" opt ; do
+      case $opt in
+         o) array+=("$OPTARG") ;;
+         f) variable="$OPTARG" ;;
+         n) flag+=1 ;;
+         h) usage ;;
+        \?) error "Unknown option: -$OPTARG" ; usage ; exit 2 ;;
+         :) error "-$OPTARG requires an argument." ; usage ; exit 2 ;;
+      esac
+    done
+  fi
+  shift $((OPTIND-1)) # Cleaning up arguments
+
+}
+
 function function_name {
 # Function purpose
 echo
@@ -46,4 +71,5 @@ echo
 
 ### MAIN ###
 check_dependencies
+parse_arguments
 function_name
